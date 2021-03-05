295cc1d2fae25b8d3b3ddc6ffe18c232e27a8410
Yoram Hekma <yoram.hekma@tnt-digital.com>
2021-03-05 14:16:29 +0100

    working with faster walking

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/yhekma/excluder
 go 1.16
 
 require (
+	github.com/karrick/godirwalk v1.16.1
 	github.com/pkg/xattr v0.4.3
+	github.com/sirupsen/logrus v1.8.0
 	gopkg.in/yaml.v2 v2.4.0
 )
--- main.go
+++ main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/karrick/godirwalk"
 	"github.com/pkg/xattr"
+	log "github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
-	"log"
 	"os"
 	"path/filepath"
 	"strings"
 )
+
+func logInit() {
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.InfoLevel)
+}
 
 type Config struct {
 	Dirnames []string `yaml:"direxcludes"`
@@ -33,51 +40,62 @@ func checkInSlice(item string, slice []string) bool {
 }
 
 func walkAll(root string, extensions, dirnames []string) error {
-	e := filepath.Walk(root, func(path string, info os.FileInfo, e error) error {
-		if e != nil {
-			return e
-		}
+	e := godirwalk.Walk(root, &godirwalk.Options{
+		ErrorCallback: func(path string, err error) godirwalk.ErrorAction {
+			return godirwalk.SkipNode
+		},
+		Callback: func(path string, de *godirwalk.Dirent) error {
+			if de.IsDir() {
+				for _, d := range dirnames {
+					if de.Name() == d {
+						log.Debugf("excluding from backup: %s", path)
+						e := xattr.Set(path, "com.apple.metadata:com_apple_backup_excludeItem", []byte("com.apple.backupd"))
+						checkErr(e, fmt.Sprint("could not set attribute on", path))
+					}
+				}
+			}
 
-		if info.Mode().IsDir() {
-			for _, d := range dirnames {
-				if info.Name() == d {
-					e = xattr.Set(path, "com.apple.metadata:com_apple_backup_excludeItem", []byte("com.apple.backupd"))
-					fmt.Println("setting: ", path)
-					checkErr(e, fmt.Sprint("could not set attribute on", path))
+			if de.IsRegular() {
+				parts := strings.Split(path, string(filepath.Separator))
+				for _, p := range parts {
+					if checkInSlice(p, dirnames) {
+						return godirwalk.SkipThis
+					}
+				}
+				for _, ext := range extensions {
+					if strings.HasSuffix(de.Name(), ext) {
+						log.Debugf("excluding from backup: %s", path)
+						e := xattr.Set(path, "com.apple.metadata:com_apple_backup_excludeItem", []byte("com.apple.backupd"))
+						checkErr(e, fmt.Sprint("could not set attribute on", path))
+					}
 				}
 			}
-		}
-
-		if info.Mode().IsRegular() {
-			process := true
-			parts := strings.Split(path, string(filepath.Separator))
-			for _, p := range parts {
-				if checkInSlice(p, dirnames) {
-					process = false
-				}
-			}
-			for _, ext := range extensions {
-				if strings.HasSuffix(info.Name(), ext) && process {
-					fmt.Println("setting: ", path)
-					e = xattr.Set(path, "com.apple.metadata:com_apple_backup_excludeItem", []byte("com.apple.backupd"))
-					checkErr(e, fmt.Sprint("could not set attribute on", path))
-				}
-			}
-		}
-		return nil
+			return nil
+		},
 	})
 	return e
 }
 
 func main() {
+	logInit()
 	var e error
 	var config Config
 	cfg := flag.String("config", "excluder.yaml", "yaml file with config")
+	verb := flag.Bool("verbose", false, "run in verbose mode")
 	flag.Parse()
+	root := flag.Arg(0)
+	if *verb {
+		log.SetLevel(log.DebugLevel)
+		log.Debug("running in debug mode as specified")
+	}
 
+	log.Debugf("reading config file %s", *cfg)
 	f, e := ioutil.ReadFile(*cfg)
 	checkErr(yaml.Unmarshal(f, &config), "could not unmarshal")
+	log.Debugf("parsed configfile, excluded directories: %s", config.Dirnames)
+	log.Debugf("parsed configfile, excluded extensions: %s", config.Extensions)
+	log.Debugf("using root dir: %s", root)
 
-	e = walkAll("tests", config.Extensions, config.Dirnames)
+	e = walkAll(root, config.Extensions, config.Dirnames)
 	checkErr(e, "could not walk")
 }
